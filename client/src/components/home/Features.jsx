88d388c4995bc8becb00dc5f219d43c0c4d438c6
pokemon <pokemon30blade@gmail.com>
2026-09-02 20:43:48 +0530

    home page design

--- client/src/components/home/Features.jsx
+++ client/src/components/home/Features.jsx
@@ -16,7 +16,7 @@ const Features = () => {
 
             <Title title='Build your resume' description='Our streamlined process helps you create a professional resume in minutes with intelligent AI-powered tools and features.' />
             
-            <div className="flex flex-col md:flex-row items-center justify-center">
+            <div className="flex flex-col md:flex-row items-center justify-center xl:-mt-10">
                 <img className="max-w-2xl w-full xl:-ml-32" src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/features/group-image-1.png" alt="" />
                 <div className="px-4 md:px-0" onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)}>
                     <div className={"flex items-center justify-center gap-6 max-w-md group cursor-pointer"}>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,14 +2,20 @@ import React from 'react'
 import Banner from '../components/home/Banner'
 import HeroSection from '../components/home/HeroSection'
 import Features from '../components/home/Features'
+import Testimonial from '../components/home/Testimonial'
+import CallToAction from '../components/home/CallToAction'
+import Footer from '../components/home/Footer'
 
 function Home() {
   return (
-    <div>
+    <>
       <Banner />
       <HeroSection />
       <Features />
-    </div>
+      <Testimonial />
+      <CallToAction />
+      <Footer />
+    </>
   )
 }
 
