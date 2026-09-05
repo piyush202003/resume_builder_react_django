1ffa7b60ae6b70dc0f2101c64993b51ef27e21da
pokemon <pokemon30blade@gmail.com>
2026-09-05 23:21:31 +0530

    resume preview done

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -3,6 +3,7 @@ import { Link, useParams } from "react-router-dom"
 import { dummyResumeData } from "../assets/assets"
 import { ArrowLeftIcon, Briefcase, ChevronLeft, ChevronRight, FileText, FolderIcon, GraduationCap, Sparkles, User } from "lucide-react"
 import PersonalInfoForm from "../components/PersonalInfoForm"
+import ResumePreview from "../components/ResumePreview"
 
 const ResumeBuilder = () => {
 
@@ -100,6 +101,7 @@ const ResumeBuilder = () => {
             </div>
 
             {/* resume preview */}
+            <ResumePreview data={resumeData} template={resumeData.template} accentColor={resumeData.accent_color} />
           </div>
         </div>
       </div>
